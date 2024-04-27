96e2b66eabce66b9b42b4dc95e02df2db1e3bc1b
nassahel <nassahel.elias@gmail.com>
2024-04-27 17:33:10 -0300

    se cambio los datos de pedido

--- src/models/pedidos.js
+++ src/models/pedidos.js
@@ -1,52 +1,49 @@
 const {Schema, model}= require('mongoose')
 
-const menuItemSchema = new Schema({
-  id:{
-    type:String,
-    required:[true]
-  },
-  name: {
-    type: 
-    String,required:[true]
-  },
-  quantity: { 
-    type: Number, 
-    required: true 
-  },
-  price: {
-    type: Number,
-    required:[true]
-  },
-  category: {
-    type: String,
-    required:[true]
-  },
-  image: {
-    type: String,
-    required:[true]
-  }
-});
+// const menuItemSchema = new Schema({
+//   id:{
+//     type:String,
+//     required:[true]
+//   },
+//   name: {
+//     type: 
+//     String,required:[true]
+//   },
+//   quantity: { 
+//     type: Number, 
+//     required: true 
+//   },
+//   price: {
+//     type: Number,
+//     required:[true]
+//   },
+//   category: {
+//     type: String,
+//     required:[true]
+//   },
+//   image: {
+//     type: String,
+//     required:[true]
+//   }
+// });
 
 const OrderSchema = Schema({
-  user: { 
-    type: Schema.Types.ObjectId, 
-    ref: 'usuario' 
-  },
-  date: { 
+
+  fecha: { 
     type: Date, 
     default: Date.now 
   },
-  order: { 
-    type: [menuItemSchema],
+  productos: { 
+    type: [],
     required: true
   },
-  status: { 
-    type: String, 
-    enum: ['pendiente', 'realizado'], 
-    default: 'pendiente', 
-    required: true 
-  },
-  totalCost: { 
+  // status: { 
+  //   type: String, 
+  //   enum: ['pendiente', 'realizado'], 
+  //   default: 'pendiente', 
+  //   required: true 
+  // },
+  total: { 
     type: Number, 
     required: true 
   },
